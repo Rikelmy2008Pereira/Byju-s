7127ce01fccbc3ce38110c9339e5e81a6087cde8
Rikelmy2008Pereira <107949157+Rikelmy2008Pereira@users.noreply.github.com>
2022-09-04 20:12:12 -0300

    Add files via upload

--- MatterJSBoilerPlate-PROJETO-Rikelmy/sketch.js
+++ MatterJSBoilerPlate-PROJETO-Rikelmy/sketch.js
@@ -1,72 +1,64 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-let world;
+
+
 let engine;
-var block1;
-var block2;
-var block3;
+let world;
+var ball;
 var ground;
+var wedge;
+var angle=60;
+var poly;
+var ball_2;
 
 
 function setup() {
-
-	createCanvas(800, 800);
-	engine = Engine.create();
-	world = engine.world;
-
-	//Crie os Corpos Aqui.
-
-	var block1_options = {
-		restitution: 0.10,
-		friction: 0.20,
-		frictionAir:1.0
-	}
-	var block2_options = {
-		restitution : 1.0,
-		friction:2.0,
-		frictionAir:0.10
-	}
-	var block3_options = {
-		restitution : 9.5,
-		friction: 0.25,
-		frictionAir: 1.0
-	}
-	var ground_options = {
-		isStatic:true
-	}
-
-	ground = Bodies.rectangle(400,790,800,10,ground_options);
-	World.add(world,ground);
-
-	block1 = Bodies.circle(220,50,10,10,block1_options);
-	World.add(world,block1);
-
-	block2 = Bodies.rectangle(110,50,10,10,block2_options);
-	World.add(world,block2);
-
-	block3 = Bodies.circle(350,50,10,10,block3_options);
-	World.add(world,block3);
-
-	Engine.run(engine);
   
-}
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
 
+  var ball_options = {
+    restitution : 0.95,
+    frictionAir : 0.01
+  }
 
-function draw() {
+  var ground_options = {
+    isStatic : true
+  }
+
+  var ball2_options = {
+    restitution : 1.0,
+    frictionAir : 0.10
+
+  }
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball_2 = Bodies.circle(350,10,20,ball2_options);
+  World.add(world,ball_2);
+  
   rectMode(CENTER);
   ellipseMode(RADIUS);
-  fill('green');
-  background(0);
-  Engine.update(engine);
-  
-  rect(ground.position.x,ground.position.y,800,10);
-  rect(block1.position.x,block1.position.y,10,10);
-  ellipse(block2.position.x,block2.position.y,10,10);
-  rect(block3.position.x,block3.position.y,10,10);
+  fill('red');
 }
 
 
+function draw() 
+{
+ background ('yellow');
+ Engine.update(engine);
+
+ ellipse(ball.position.x,ball.position.y,20);
+ ellipse(ball_2.position.x,ball_2.position.y,20);
+ rect(ground.position.x,ground.position.y,400,20);
+ 
+  
+}
 
